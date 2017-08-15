84861cf62448345d6352b56e5d5a8e96f21b2906
Adam Bretz <arbretz@gmail.com>
2017-08-15 13:08:13 -0400

    Added type def file.

---
+++ lib/index.d.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+
+/**
+ * Creates a Celebrate middleware function.
+ * @param {object} schema object where each key is one of ["params", "headers", "query", "body"] and the value is
+ * a Joi schema.
+ * @param {object} config optional configuration options that will be passed directly into Joi.
+ */
+declare function Celebrate (schema: {
+    params: object,
+    headers: object,
+    query: object,
+    body: object,
+}, config?: object): express.RequestHandler;
+
+declare namespace Celebrate {
+    /**
+     * Creates a Celebrate error handler middleware function
+     */
+    function errors(): express.ErrorRequestHandler;
+}
+
+export = Celebrate;
