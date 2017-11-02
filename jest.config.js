38c51bf39ea8b989f93a40c52d2fcb4060c99b96
Adam Bretz <arbretz@gmail.com>
2017-11-02 15:36:19 -0400

    Jest migration.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: ['lib/index.js'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
