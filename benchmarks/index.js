d7870c74feebd2e5416fa1023684deb7fd11da90
Adam Bretz <arbretz@gmail.com>
2017-11-20 15:07:31 -0500

    Perf updates
    Added some benchmarking tooling to have an idea of how changes affect
    performance Moved code around to allow for more constant values and fewer memory
    allocations Switch to non-callback validation function to remove extra closure

---
+++ benchmarks/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Benchmark = require('benchmark');
+const { celebrate, Joi } = require('../lib');
+const middleware = celebrate({
+  body: {
+    name: Joi.string().allow('adam').required()
+  }
+});
+
+const suite = new Benchmark.Suite();
+const noop = () => {};
+
+suite.add('valid', () => {
+  middleware({
+    body: {
+      name: 'adam'
+    },
+    method: 'post'
+  }, {}, noop);
+}).add('invalid', () => {
+  middleware({
+    body: {},
+    method: 'post'
+  }, {}, noop);
+});
+
+suite.on('complete', function suiteComplete() {
+  for (let i = 0; i < this.length; i += 1) {
+    console.log(this[i].toString());
+  }
+});
+
+suite.run();
