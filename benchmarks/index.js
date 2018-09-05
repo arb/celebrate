f5661af5a4a1e0278d0308b405b60864c4ce1465
Adam Bretz <arbretz@gmail.com>
2018-09-05 11:18:39 -0400

    Code Cleanup and optimizations
    Closes #80
    Closes #83
    
    Removed fast-series and just did things using native loops and no callbacks. Saw
    about a 1000 ops per second speed improvement. Dropped conditional setting of `req.*` values
    and just always use `Object.defineProperty`.

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,11 +1,10 @@
-'use strict';
-
 const Benchmark = require('benchmark');
 const { celebrate, Joi } = require('../lib');
+
 const middleware = celebrate({
   body: {
-    name: Joi.string().allow('adam').required()
-  }
+    name: Joi.string().allow('adam').required(),
+  },
 });
 
 const suite = new Benchmark.Suite();
@@ -14,14 +13,14 @@ const noop = () => {};
 suite.add('valid', () => {
   middleware({
     body: {
-      name: 'adam'
+      name: 'adam',
     },
-    method: 'post'
+    method: 'post',
   }, {}, noop);
 }).add('invalid', () => {
   middleware({
     body: {},
-    method: 'post'
+    method: 'post',
   }, {}, noop);
 });
 
