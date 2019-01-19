c74849c52f0267d09c75c88d1e842e6f216ca990
Adam Bretz <arbretz@gmail.com>
2019-01-19 12:30:06 -0500

    Use jest for eslint (#98)
    Closes #94
    
    Tap into jest runners to run eslint too so you can do everything in a single
    command. Also added is-ci-cli so you can just do "npm test" regardless of what
    environment you're in.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['lib/index.js'],
-  coverageDirectory: './coverage',
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+  projects: [{
+    displayName: 'test',
+    collectCoverage: true,
+    collectCoverageFrom: ['lib/index.js'],
+    coverageDirectory: './coverage',
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
     },
-  },
-  testEnvironment: 'node',
-  verbose: true,
+    testEnvironment: 'node',
+    verbose: true,
+  }, {
+    runner: 'jest-runner-eslint',
+    displayName: 'linter',
+  }],
+  watchPlugins: ['jest-runner-eslint/watch-fix'],
 };
