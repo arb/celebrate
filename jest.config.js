1aef03c8350de4ef89b5c408dc06fae386f1a111
Adam Bretz <arbretz@gmail.com>
2019-02-28 09:50:59 -0500

    Testing cleanup
    Fixed coverage collection issue.
    Fixed eslint issue.
    Tweeked local vs ci test commands.

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,23 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/lib/index.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   projects: [{
     displayName: 'test',
-    collectCoverage: true,
-    collectCoverageFrom: ['lib/index.js'],
-    coverageDirectory: './coverage',
-    coverageThreshold: {
-      global: {
-        branches: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
-      },
-    },
     testEnvironment: 'node',
-    verbose: true,
   }, {
-    runner: 'jest-runner-eslint',
     displayName: 'linter',
+    runner: 'jest-runner-eslint',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/lib/**/*.js', '<rootDir>/test/**/*.js', '<rootDir>/jest.config.js'],
   }],
   watchPlugins: ['jest-runner-eslint/watch-fix'],
 };
