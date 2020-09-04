73e0e98454ea67a42ae9868abfd37744e28977d0
Adam Bretz <arbretz@gmail.com>
2020-09-04 14:41:15 -0400

    Fixed benchmarking

--- utils/benchmark.js
+++ utils/benchmark.js
@@ -1,47 +1,45 @@
 const Benchmark = require('benchmark');
 const { celebrate, Joi, Modes } = require('../lib');
 
-const schema1 = {
-  body: {
-    name: Joi.string().allow('adam').required(),
-  },
-};
-
 const suite = new Benchmark.Suite();
 const noop = () => { };
 
-suite.add('valid', () => {
-  celebrate(schema1)({
+const f = celebrate({
+  body: {
+    name: Joi.string().allow('adam').required(),
+  },
+});
+const g = celebrate({
+  body: {
+    name: Joi.string().allow('adam').required(),
+  },
+  query: {
+    age: Joi.number().integer().required(),
+  },
+  params: {
+    page: Joi.number().required(),
+  },
+}, undefined, {
+  mode: Modes.FULL,
+});
+
+suite
+  .add('valid', () => f({
     body: {
       name: 'adam',
     },
     method: 'post',
-  }, {}, noop);
-}).add('invalid partial', () => {
-  celebrate(schema1)({
+  }, {}, noop))
+  .add('invalid partial', () => f({
     body: {},
     method: 'post',
-  }, {}, noop);
-}).add('invalid full', () => {
-  celebrate({
-    body: {
-      name: Joi.string().allow('adam').required(),
-    },
-    query: {
-      age: Joi.number().integer().required(),
-    },
-    params: {
-      page: Joi.number().required(),
-    },
-  }, undefined, {
-    mode: Modes.FULL,
-  })({
+  }, {}, noop))
+  .add('invalid full', () => g({
     method: 'post',
     body: {},
     query: {},
     params: {},
-  }, {}, noop);
-});
+  }, {}, noop));
 
 suite.on('complete', function suiteComplete() {
   for (let i = 0; i < this.length; i += 1) {
@@ -50,3 +48,9 @@ suite.on('complete', function suiteComplete() {
 });
 
 suite.run({ async: true });
+
+// first run
+// valid x 707,830 ops/sec ±7.21% (69 runs sampled)
+// invalid partial x 581,854 ops/sec ±8.39% (36 runs sampled)
+// invalid full x 15,922 ops/sec ±8.31% (67 runs sampled)
+// --------
